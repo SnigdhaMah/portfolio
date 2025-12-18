1226da53ee2588fd3722f7bbae8a168b9bbb3cd1
SnigdhaM <smileyramya04@gmail.com>
2025-12-18 15:54:01 -0800

    rename + add sanity

---
+++ portfolio-data/sanity.cli.ts
@@ -0,0 +1,15 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kwh5qxt2',
+    dataset: 'experience'
+  },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/cli#auto-updates
+     */
+    autoUpdates: true,
+  }
+})
