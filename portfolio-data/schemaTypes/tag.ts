aeffaa52a500942b6b1e821aa71bde90df31cc88
SnigdhaM <smileyramya04@gmail.com>
2025-12-19 11:14:34 -0800

    update sanity types

--- portfolio-data/schemaTypes/tag.ts
+++ portfolio-data/schemaTypes/tag.ts
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'category',
-  title: 'Category',
+  name: 'tag',
+  title: 'Tag',
   type: 'document',
   fields: [
     defineField({
@@ -11,9 +11,9 @@ export default defineType({
       type: 'string',
     }),
     defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: 'iconName',
+      title: 'Icon Name',
+      type: 'icon',
     }),
   ],
 })
