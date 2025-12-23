cad1fd25ba3351b370ff6701319c05d1e2c7b141
SnigdhaM <smileyramya04@gmail.com>
2025-12-22 19:14:50 -0800

    delete the extra folders

--- portfolio-data/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import blockContent from './blockContent'
-import tag from './tag'
-import experience from './experience'
-import icon from './icon'
-
-export const schemaTypes = [experience, tag, blockContent, icon]
--- website/app/sanity/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: 'kwh5qxt2',
-  dataset: 'experience',
-  apiVersion: "2024-01-01",
-  useCdn: false,
-});
