80489aac800fb2d1531e8c1a76cec0d814755ad0
SnigdhaM <smileyramya04@gmail.com>
2026-01-02 13:56:44 -0800

    redeploy render

--- portfolio-website/app/page.tsx
+++ portfolio-website/app/page.tsx
@@ -3,12 +3,15 @@ import { client } from "./sanity/client";
 import Home from "./home";
 import { ALL_EXP_QUERY, FILE_QUERY, TAG_QUERY } from "./sanity/query";
 
+export const revalidate = 200; // revalidate results after 200 seconds
+
 export default async function IndexPage() {
   // Query Sanity in asynchronous server component
   const experiences = await client.fetch<SanityDocument[]>(ALL_EXP_QUERY, {}, {});
   const allTags = await client.fetch<SanityDocument[]>(TAG_QUERY, {}, {});
   const resume = await client.fetch(FILE_QUERY, { tagVal: "Resume" }, {});
   const transcript = await client.fetch(FILE_QUERY, { tagVal: "Transcript" }, {});
+  console.log("Exp from Sanity:" + experiences)
   
   // then pass the info into the client component to have access to useStates
   return <Home experiences={experiences} allTags={allTags} resume={resume.url as string} transcript={transcript.url as string}/>
